dba5793948a03094fb297972120c2a7fcb2ea3ce
James Smith <James@gaslight.co>
2014-01-06 21:07:25 -0500

    upgrade to the latest elixir

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule YahtzeeKata.Mixfile do
   def project do
     [ app: :yahtzee_kata,
       version: "0.0.1",
-      elixir: "~> 0.11.2",
+      elixir: "~> 0.12",
       deps: deps ]
   end
 
