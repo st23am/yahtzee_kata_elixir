c2a589ef60b065262780b966135e05144397b82c
James Smith <James@gaslight.co>
2014-01-06 21:09:33 -0500

    Score Pairs with tests

--- lib/yahtzee_kata.exs
+++ lib/yahtzee_kata.exs
@@ -2,12 +2,15 @@ defmodule Yahtzee do
   def score('Pair', rolls) do
     Enum.sort(rolls)
     |> score_pairs([])
+    |> sum
   end
   
   def score(category, rolls) do
     match_roll(rolls, category_to_number(category), [])
     |> sum
   end
+
+  defp score_pairs([], []), do: 0
 
   defp score_pairs([], pairs) do
     2 * Enum.max(pairs)
@@ -20,10 +23,10 @@ defmodule Yahtzee do
     else
       found_pairs = pairs
     end
-    find_pairs(tail, found_pairs)
+    score_pairs(tail, found_pairs)
   end
 
-  defp match_roll([], roll_to_match, matches), do: matches
+  defp match_roll([], _roll_to_match, matches), do: matches
 
   defp match_roll([head | tail], roll_to_match, matches) when head == roll_to_match do
     match_roll(tail, roll_to_match, Enum.concat(matches, [head]))
@@ -36,7 +39,7 @@ defmodule Yahtzee do
   defp sum(rolls), do: _sum(rolls, 0)
   defp _sum([ head | tail ], total), do: _sum(tail, head + total)
   defp _sum([], total), do: total
-
+  defp _sum(number, 0), do: number
 
   defp category_to_number('Ones'),   do: 1
   defp category_to_number('Twos'),   do: 2
--- test/yahtzee_kata_test.exs
+++ test/yahtzee_kata_test.exs
@@ -1,7 +1,47 @@
 defmodule YahtzeeKataTest do
   use ExUnit.Case
 
-  test "the truth" do
-    assert(true)
+  test "scoring Ones through sixes" do
+    assert(Yahtzee.score('Ones',   [1,1,2,4,6]) == 2)
+    assert(Yahtzee.score('Twos',   [1,2,2,2,4]) == 6)
+    assert(Yahtzee.score('Threes', [3,2,3,2,3]) == 9)
+    assert(Yahtzee.score('Fours',  [1,4,2,2,4]) == 8)
+    assert(Yahtzee.score('Fives',  [1,5,3,5,4]) == 10)
+    assert(Yahtzee.score('Sixes',  [1,6,4,5,6]) == 12)
   end
+  
+  test "Ones through Sixes with no match" do
+    assert(Yahtzee.score('Ones',   [2,3,3,4,6]) == 0)
+    assert(Yahtzee.score('Twos',   [1,3,3,1,4]) == 0)
+    assert(Yahtzee.score('Threes', [1,2,4,4,5]) == 0)
+    assert(Yahtzee.score('Fours',  [1,5,2,2,3]) == 0)
+    assert(Yahtzee.score('Fives',  [1,2,3,4,4]) == 0)
+    assert(Yahtzee.score('Sixes',  [1,3,4,2,4]) == 0)
+  end
+
+  test "scoring simple pairs" do
+    assert(Yahtzee.score('Pair', [2,1,2,4,5]) == 4)
+  end
+  
+  test 'scoring highest pair only' do
+    assert(Yahtzee.score('Pair', [2,1,2,4,4]) == 8)
+  end
+  
+  test 'highest pair more than 2 matches' do
+    assert(Yahtzee.score('Pair', [2,4,2,4,4]) == 8)
+  end
+
+  test "scoring pairs with no matches" do
+    assert(Yahtzee.score('Pair', [1,2,5,3,4]) == 0)
+  end
+
+  test "scoring two pair" do
+  end
+
+  test "scoring two pair with no match" do
+  end
+
+  test "scoring two pair with multiple matches uses highest" do
+  end
+
 end
